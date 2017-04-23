adae6bd12412eee369653b090c74a194eee0ab1c
Max Feng <max.mf.feng@gmail.com>
2017-04-22 19:23:00 -0700

    a

--- vis/Particles.cpp
+++ vis/Particles.cpp
@@ -12,7 +12,7 @@ Particles::Particles()
             for(int z=0; z<nz; z++)
             {
               ParticleGridCube pgc;
-              if ((abs(x - 0) + abs(y - 0) + abs(z - 0)) < 5) {
+              if ((abs(x - 3) + abs(y - 3) + abs(z - 3)) < 5) {
                 pgc.density = 5.0;
               } else {
                 pgc.density = 0.0;
@@ -30,7 +30,7 @@ void Particles::render() const
     int nz = 30;
     GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat mat_shininess[] = { 50.0 };
-    GLfloat light_position[] = { 10.0, 10.0, 10.0, 0.0 };
+    GLfloat light_position[] = { -10.0, -10.0, -10.0, 0.0 };
     glShadeModel (GL_SMOOTH);
     glPushAttrib(GL_ALL_ATTRIB_BITS);
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
@@ -41,12 +41,10 @@ void Particles::render() const
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_COLOR_MATERIAL);
-    glColorMaterial(GL_FRONT, GL_DIFFUSE);
-    glColor3f(0.2, 0.5, 0.8);
-    glColorMaterial(GL_FRONT, GL_SPECULAR);
-    glColor3f(0.9, 0.9, 0.9);
-    glColorMaterial(GL_FRONT, GL_AMBIENT);
-    glColor3f(0.2, 0.5, 0.8);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
     for(int x=0; x<nx; x++)
     {
         for(int y=0; y<ny; y++)
@@ -54,12 +52,16 @@ void Particles::render() const
             for(int z=0; z<nz; z++)
             {
               glPushMatrix();
+              glScalef(0.5, 0.5, 0.5);
               glTranslatef(x, y, z);
-              glutSolidSphere(particles[x * ny * nz + y * nz + z].density / 30, 10, 10);
+              int i = x * ny * nz + y * nz + z;
+              glColor4f(1.0, 1.0, 1.0, particles[i].density / 5.0);
+              glutSolidCube(0.9);
               glPopMatrix();
             }
         }
     }
+    glDisable(GL_BLEND);
 
     glPopAttrib();
 }
@@ -110,10 +112,11 @@ void Particles::step(int elapsed_time)
             }
 
             // Compute amount to diffuse
-            double diffuseAmount = particles[i].density / diffuseTo.size();
+            double diffuseAmount = particles[i].density * 0.1 / diffuseTo.size();
             for (int j = 0; j < diffuseTo.size(); j++) {
               particles[diffuseTo[j]].new_density += diffuseAmount;
             }
+            particles[i].density -= diffuseAmount * diffuseTo.size();
           }
       }
   }
@@ -125,7 +128,7 @@ void Particles::step(int elapsed_time)
           for(int z=0; z<nz; z++)
           {
             int i = x * ny * nz + y * nz + z;
-            particles[i].density = particles[i].new_density;
+            particles[i].density += particles[i].new_density;
             t_density += particles[i].density;
           }
       }
